b9b571a7d39d609671d6b0e5d6ebb6e12c9e94bf
AnatolySoroka <toliksoroka66@gmail.com>
2023-02-05 15:49:07 +0200

    task-01 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,67 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItems(galleryItems);
+
+function createGalleryItems(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                 <img
+                    loading="lazy"
+                    class="gallery__image lazyload"
+                    data-src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>
+        `
+    }).join('');
+}
+
+galleryEl.innerHTML = galleryMarkup;
+
+galleryEl.addEventListener('click', onClick);
+
+function onClick(event) {
+    //Відміняєм стандартну поведінку браузера
+    event.preventDefault();
+
+    //Перевіряєм чи натискаєм саме на картинку
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    //Відкриваєм модальне вікно
+    const instance = basicLightbox.create(`
+        <img src="${event.target.dataset.source}" width="800" height="600">
+    `);
+    instance.show();
+
+    //Закриваєм модальне вікно при натисканні клавіші Esc
+    window.addEventListener('keydown', (event) => {
+        if (event.code === 'Escape') {
+            instance.close();
+        }
+    }, { once: true })
+};
+
+
+//Підключаєм lazyload images
+if ('loading' in HTMLImageElement.prototype) {
+    const images = document.querySelectorAll('img[data-src]');
+    images.forEach(image => {
+        image.src = image.dataset.src;
+    });
+} else {
+    const script = document.createElement('script');
+    script.scr = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
+    script.integrity = 'sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==';
+    script.crossorigin = 'anonymous" referrerpolicy="no-referrer';
+
+    document.body.appendChild(script);
+}
+
--- js/gallery-items.js
+++ js/gallery-items.js
@@ -62,4 +62,193 @@ export const galleryItems = [
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
     description: 'Lighthouse Coast Sea',
   },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014_960_720.jpg',
+    description: 'Flowers',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2015/06/19/21/24/avenue-815297__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2015/06/19/21/24/avenue-815297_960_720.jpg',
+    description: 'Avenue',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/14/04/45/elephant-1822636__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/14/04/45/elephant-1822636_960_720.jpg',
+    description: 'Elephant',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2012/06/19/10/32/owl-50267__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2012/06/19/10/32/owl-50267_960_720.jpg',
+    description: 'Owl',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2013/10/02/23/03/mountains-190055__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2013/10/02/23/03/mountains-190055_960_720.jpg',
+    description: 'Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2015/07/05/10/18/tree-832079__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2015/07/05/10/18/tree-832079_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2013/04/04/12/34/mountains-100367__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2013/04/04/12/34/mountains-100367_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2013/08/20/15/47/poppies-174276__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2013/08/20/15/47/poppies-174276_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/08/11/23/48/mountains-1587287__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/08/11/23/48/mountains-1587287_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/08/11/23/48/mountains-1587287__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/08/11/23/48/mountains-1587287_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/11/17/22/15/trees-3822149__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/11/17/22/15/trees-3822149_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/01/08/11/57/butterflies-1127666__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/01/08/11/57/butterflies-1127666_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2014/08/01/00/08/pier-407252__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2014/08/01/00/08/pier-407252_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/03/27/22/22/fox-1284512__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/03/27/22/22/fox-1284512_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/02/13/23/41/nature-3151869__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/02/13/23/41/nature-3151869_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/23/13/48/beach-1852945__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/23/13/48/beach-1852945_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/04/18/22/05/seashells-1337565__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/04/18/22/05/seashells-1337565_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2013/07/25/13/01/stones-167089__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2013/07/25/13/01/stones-167089_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/04/09/09/56/avenue-2215317__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/04/09/09/56/avenue-2215317_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2015/04/19/08/33/flower-729512__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2015/04/19/08/33/flower-729512_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2013/11/28/10/03/river-219972__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2013/11/28/10/03/river-219972_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/08/21/23/29/forest-3622519__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/08/21/23/29/forest-3622519_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/12/15/13/51/polynesia-3021072__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/12/15/13/51/polynesia-3021072_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2013/07/25/01/31/forest-166733__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2013/07/25/01/31/forest-166733_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/05/08/13/15/bird-2295436__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/05/08/13/15/bird-2295436_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/12/05/11/39/fox-1883658__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/12/05/11/39/fox-1883658_960_720.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
 ];
